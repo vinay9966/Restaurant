b8a27178b4e77475ce18e234bf1124c3d9641091
vinay9966 <62562719+vinay9966@users.noreply.github.com>
2020-05-03 20:38:31 -0700

    added restauranr search app

---
+++ Restaurant-search/src/Utils/utilities.js
@@ -0,0 +1,18 @@
+let gradient = [
+];
+
+let sliderWidth = 200;
+
+export const getColorFromGradient = (value) => {
+   
+            return false;
+  
+};
+
+const pickHex = (color1, color2, weight) =>{
+   
+    return null;
+}
+
+export const MapStyles = [
+  ]
